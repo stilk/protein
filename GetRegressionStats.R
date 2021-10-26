972d54b2e5d2ee072c8b55d22230428b705892a0
Susanne Tilk <susannetilk@gmail.com>
2021-10-26 08:35:33 -0700

    add option to normalize Y values for extenstion beyond exp

--- GetRegressionStats.R
+++ GetRegressionStats.R
@@ -12,27 +12,35 @@ NormalizeValues = function(df) {
 } 
 
 
-DoMixedEffectRegression = function(df) {
+DoMixedEffectRegression = function(df, NormalizeY) {
     # Does mixed effect regression of Values by LogScore (i.e. mutational load) while
     # controlling for cancer type and tumor purity. 
-    model = lmer(NormalizedValue ~ NormalizedMutLoad + (1 | type) + (1 |purity), data=df)
+    if (NormalizeY) {
+         model = lmer(NormalizedValue ~ LogScore + (1 | type) + purity, data=df)
+    } else {
+         model = lmer(Value ~ LogScore + (1 | type) + purity, data=df)
+    }
     return(data.frame(summary(model)[10]$coefficients, summary(model)[11]))
 }
 
 DoMixedEffectRegressionWithGeneName = function(df) {
-    model = lmer(NormalizedValue ~ LogScore + (1 | type) + (1 |purity) + GeneName, data=df)
+    model = lmer(NormalizedValue ~ LogScore + (1 | type) + purity + GeneName, data=df)
     return(data.frame(summary(model)[10]$coefficients, summary(model)[11]))
 }
 
-DoLinearRegression = function(df) {
-    model = lm(NormalizedValue ~ LogScore, data=df)
+DoLinearRegression = function(df, NormalizeY) {
+    if (NormalizeY) {
+        model = lm(NormalizedValue ~  LogScore, data=df)
+    } else {
+        model = lm(Value ~  LogScore, data=df)
+    }
     f = summary(model)$fstatistic
     pVal = pf(f[1],f[2],f[3],lower.tail=F) # p value for the overall fit of the model againt null that R2=0
     return(data.frame(summary(model)[9], summary(model)[6], summary(model)[4]$coefficients, pVal))
 }
 
 
-DoRegressionPerGene = function(df, RegressionType) {
+DoRegressionPerGene = function(df, RegressionType, NormalizeY) {
     RegressionResults = data.frame()
     AllGenes = unique(df$GeneName)
     for (i in 1:length(AllGenes)) {
@@ -41,17 +49,15 @@ DoRegressionPerGene = function(df, RegressionType) {
         GeneName = unique(SingleGene$GeneName)
         tryCatch( 
         {  if (RegressionType == 'MixedEffect') {
-                RegressionResults = rbind(RegressionResults, data.frame(DoMixedEffectRegression(SingleGene), GeneName))
+                RegressionResults = rbind(RegressionResults, data.frame(DoMixedEffectRegression(SingleGene, NormalizeY), GeneName))
         } else if (RegressionType == 'OLS') {
-                RegressionResults = rbind(RegressionResults, data.frame(DoLinearRegression(SingleGene), GeneName))
+                RegressionResults = rbind(RegressionResults, data.frame(DoLinearRegression(SingleGene, NormalizeY), GeneName))
         }}, 
             error=function(error_message) { 
             message(error_message)
             print(paste0("Couldn't calculate regression statistics for ", AllGenes[i]))
         })
     }
-    # Calculate adjusted p-values for multiple hypothesis testing
-    RegressionResults$adj.pval = p.adjust(RegressionResults$Pr...t.., method= 'fdr')
     return(RegressionResults)
 }
 
@@ -65,7 +71,5 @@ DoRegressionPerGroup = function(df) {
         message(error_message) # Print error statement
         print(paste0("Couldn't calculate regression statistics for group."))
     })
-    # Calculate adjusted p-values for multiple hypothesis testing
-    RegressionResults$adj.pval = p.adjust(RegressionResults$Pr...t.., method= 'fdr')
     return(RegressionResults)
 }
