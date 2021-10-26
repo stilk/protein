9d85907ad8640bea3eb0fae1adc829e28c52d7ca
Susanne Tilk <susannetilk@gmail.com>
2021-10-26 08:39:02 -0700

    add protein + as data

--- CalculateMetrics.py
+++ CalculateMetrics.py
@@ -49,50 +49,95 @@ def GetRegressionStatsInput(Dataset, DataType, MutType):
         Values = GetDrugResponseData(Screen='primary')
     elif DataType == 'Expression':
         Values = GetExpressionData(Dataset)
+    elif DataType == 'Protein':
+        Values = GetProteinExpressionData(Dataset)
+    elif DataType == 'AS':
+        Values = GetAlternativeSplicingData()
     Muts = AnnotateMutationalLoad(GetMutations(Dataset), MutType=MutType)
-    Muts['NormalizedMutLoad'] = (Muts['MutLoad'] - Muts['MutLoad'].mean()) / Muts['MutLoad'].std()
+    #Muts['NormalizedMutLoad'] = (Muts['MutLoad'] - Muts['MutLoad'].mean()) / Muts['MutLoad'].std()
     Purity = GetTumorPurity()
     Tissue = GetTissueType(Dataset)
+    if DataType == 'AS':
+        Muts['Barcode'] = Muts['Barcode'].str[0:12]
     Stats = Values.merge(Muts, left_on='Barcode', right_on='Barcode')
-    #Stats['LogScore'] = np.log10(Stats['MutLoad'] + 1)
+    Stats['LogScore'] = np.log10(Stats['MutLoad'] + 1)
     if Dataset == 'TCGA':
         Covariates = Tissue.merge(Purity, left_on='Barcode', right_on='Barcode')
+        if DataType == 'AS':
+            Covariates['Barcode'] = Covariates['Barcode'].str[0:12]
     else:
         Covariates = Tissue
     return(Stats.merge(Covariates, left_on='Barcode', right_on='Barcode'))
 
 
-def GetRegressionEstimates(Dataset, DataType, MutType='SNV', GeneSet=[], ByGene=True):
+def GetRegressionEstimates(Dataset, DataType, MutType='SNV', GeneSet=[], ByGene=True, NormalizeY=True):
     SetUpPlottingPackages()
     df = GetRegressionStatsInput(Dataset, DataType, MutType)
     if len(GeneSet) != 0: # Subset to genes of interest
         df = df[df['GeneName'].isin(GeneSet)]
     R = ConvertPandasDFtoR(df)
     if ByGene:
-        if Dataset == 'TCGA':
-            return(ConvertRDataframetoPandas(ro.r.DoRegressionPerGene(R, 'MixedEffect')))
+        if DataType == 'Protein' or DataType == 'AS':
+            return(ConvertRDataframetoPandas(ro.r.DoRegressionPerGene(R, 'OLS', False)))
         elif Dataset == 'CCLE':
-            return(ConvertRDataframetoPandas(ro.r.DoRegressionPerGene(R, 'OLS')))
+            return(ConvertRDataframetoPandas(ro.r.DoRegressionPerGene(R, 'OLS', True)))
+        elif Dataset == 'TCGA':
+            return(ConvertRDataframetoPandas(ro.r.DoRegressionPerGene(R, 'MixedEffect', True)))
         elif Dataset == 'GTEX':
             return(ConvertRDataframetoPandas(ro.r.DoRegressionPerGene(R, 'OLS')))
     else: # Do regression by gene group
         if Dataset == 'TCGA':
             return(ConvertRDataframetoPandas(ro.r.DoRegressionPerGroup(R)))
 
-def GetRegressionToGeneSetsOfInterest(Group, Dataset, DataType, MutType):
-    Dir = os.getcwd() + '/GeneSets/'
-    RegressionResults = pd.DataFrame()
-    if Group == 'Chaperome':
-        set = pd.read_csv(Dir + 'Human_Chaperome_TableS1A_PMID_29293508', sep='\t').rename(columns={'Gene':'GeneName','Level2':'Group'})
-    for GeneGroup in set['Group'].unique():
-        GeneSet = set[set['Group'] == GeneGroup]['GeneName']
-        RegressionResults = RegressionResults.append(GetRegressionEstimates(Dataset, DataType, MutType, GeneSet, ByGene=False).assign(GeneGroup = GeneGroup))
-    return(RegressionResults)
+
+
+def GetDeltaPSIForEachGene():
+    Samples = pd.read_csv(os.getcwd() + '/Data/Raw/Mutations/TCGA/CancerTypesAndMutLoadForDGE')
+
+Low = GetAlternativeSplicingData(AS='RI', BarcodesOfInt=Samples[Samples['MutLoad'] <= 10]['Barcode'].tolist())
+High = GetAlternativeSplicingData(AS='RI', BarcodesOfInt=Samples[Samples['MutLoad'] >= 1000]['Barcode'].tolist())
+
+# def doTTest(row):
+# 	'''This is a two-sided test for the null hypothesis that 2 independent samples have identical average (expected) values.'''
+# 	low = row[list(set(row.index) & set(getBarcodesOfInterest('low_SNV')))].dropna()
+# 	high = row[list(set(row.index) & set(getBarcodesOfInterest('high_SNV')))].dropna()
+# 	return( pd.Series({'pVal': stats.ttest_ind(low, high)[1], 
+# 						'low_mean':low.mean(), 
+# 						'high_mean': high.mean(), 
+# 						'lowToHighDelta': low.mean() - high.mean()
+# 						})
+# 			)
+
+# def calculateDeltaPSI(splice):
+# 	splice = addPctMissing(splice)
+# 	splice = splice[splice['pct_missing'] < 0.25]
+# 	splice = splice.set_index(['symbol', 'as_id', 'splice_type', 'exons', 'from_exon', 'to_exon', 'pct_missing'])
+# 	out = splice.apply(doTTest, axis=1)
+# 	return(out)
 
 
 
-GetRegressionEstimates(Dataset='TCGA', DataType='Expression', MutType='SNV', GeneSet=[], ByGene=True).to_csv(
-    '/labs/ccurtis2/tilk/scripts/protein/Data/Regression/StandardizedExpressionMixedEffectRegressionEstimatesTCGA')
+# # GetNumberOfMutsAndCancerType(Dataset='TCGA')
+
+# GetRegressionEstimates(Dataset='TCGA', DataType='AS', MutType='KsKa', GeneSet=[], ByGene=True).to_csv(
+#     '/labs/ccurtis2/tilk/scripts/protein/Data/Regression/ASMixedEffectRegressionEstimatesKsKaTCGA')
+
+
+# GetRegressionEstimates(Dataset='TCGA', DataType='Expression', MutType='KsKa', GeneSet=[], ByGene=True).to_csv(
+#     '/labs/ccurtis2/tilk/scripts/protein/Data/Regression/ExpressionMixedEffectRegressionEstimatesKsKaTCGAPurity')
+
+# GetRegressionEstimates(Dataset='CCLE', DataType='Expression', MutType='KsKa', GeneSet=[], ByGene=True, NormalizeY=True).to_csv(
+#     '/labs/ccurtis2/tilk/scripts/protein/Data/Regression/ExpressionOLSRegressionEstimatesKsKaCCLE')
+
+GetRegressionEstimates(Dataset='CCLE', DataType='Protein', MutType='KsKa', GeneSet=[], ByGene=True, NormalizeY=False).to_csv(
+    '/labs/ccurtis2/tilk/scripts/protein/Data/Regression/ProteinOLSRegressionEstimatesKsKaCCLE')
+
+
+# GetRegressionEstimates(Dataset='CCLE', DataType='RNAi', MutType='KsKa', GeneSet=[], ByGene=True).to_csv(
+#     '/labs/ccurtis2/tilk/scripts/protein/Data/Regression/RNAiOLSRegressionEstimatesKsKaCCLE')
+
+
+#RunVEPToGetDeleteriousScores(Dataset='CCLE').to_csv('/labs/ccurtis2/tilk/scripts/protein/Data/Raw/Mutations/CCLE/CCLE_Mutations_Polyphen_Annotated', sep='\t')
 
 
 
