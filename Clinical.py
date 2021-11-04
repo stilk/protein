5403e6efdac5a688952538280647d644281ba6c4
Susanne Tilk <susannetilk@gmail.com>
2021-11-04 14:51:23 -0700

    add TCGA clinical info

--- Clinical.py
+++ Clinical.py
@@ -8,7 +8,7 @@ from rpy2.robjects.packages import importr
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
 
-def SetUpPlottingPackages():
+def SetUpSurvivalPackages():
     ggplot = importr('ggplot2')
     #scales = importr('scales')
     #ggpmisc = importr('ggpmisc')
@@ -31,6 +31,35 @@ def ConvertRDataframetoPandas(df):
         dfInPd = ro.conversion.rpy2py(df) # Convert R dataframe back to Pandas
     return(dfInPd)
 
+    pd.concat([
+        pd.DataFrame({'Group': '20S Core' , 'subgroup' : 'Proteasome', 'Hugo' : corum[corum['ComplexName'] == '20S proteasome']['subunits(Gene name)'].str.split(';', expand=True).melt()['value']  }),
+        pd.DataFrame({'Group': '19S Regulatory Particle' , 'subgroup' : 'Proteasome', 'Hugo' : ['PSMC1','PSMC2','PSMC3','PSMC4','PSMC5','PSMC6', 'PSMD1','PSMD2','PSMD3','PSMD4','PSMD6','PSMD7','PSMD8','PSMD11','PSMD12','PSMD13','PSMD14'] })
+    ])
+
+def GetTCGAClinical():
+    DataDir = os.getcwd() + '/Data/Raw/Clinical/'
+    # Get clinical info and match to TMB
+    clin = pd.read_csv(DataDir + '/TCGA/TCGA-CDR-SupplementalTableS1.txt', sep='\t')
+    Samples = pd.read_csv(os.getcwd() + '/Data/Raw/Mutations/TCGA/CancerTypesAndMutLoadForDGE')
+    Samples['Barcode'] = Samples['Barcode'].str[0:12]
+    clin = clin.merge(Samples, left_on='bcr_patient_barcode', right_on='Barcode')
+    # Get average gene expression of the proteasome and match for all cancer types 
+    exp = pd.read_csv('/labs/ccurtis2/tilk/scripts/protein/Data/AS_Tables/TCGA_AvgGeneExpForAllCancerTypes')
+    exp['Barcode'] = exp['Barcode'].str[0:12]
+    corum = pd.read_csv('/labs/ccurtis2/tilk/02_DNDS/separateDatabaseFiles/CORUM/coreComplexes.txt.zip', sep='\t')
+    Proteasome = pd.concat([
+        pd.DataFrame({'Group': '20S Core' , 'subgroup' : 'Proteasome', 'Hugo' : corum[corum['ComplexName'] == '20S proteasome']['subunits(Gene name)'].str.split(';', expand=True).melt()['value']  }),
+        pd.DataFrame({'Group': '19S Regulatory Particle' , 'subgroup' : 'Proteasome', 'Hugo' : ['PSMC1','PSMC2','PSMC3','PSMC4','PSMC5','PSMC6', 'PSMD1','PSMD2','PSMD3','PSMD4','PSMD6','PSMD7','PSMD8','PSMD11','PSMD12','PSMD13','PSMD14'] })
+    ])
+    # Set a threshold that if half of proteasomal genes are over-expressed, we'll call this 'over-expressed' and vice versa
+    exp=exp.merge(Proteasome, left_on='GeneName', right_on='Hugo')
+    exp.to_csv('/labs/ccurtis2/tilk/scripts/protein/Data/AS_Tables/TCGA_AvgGeneExpInProteasomeForAllCancerTypes')
+    thresh = exp.groupby(['Barcode','Group'])[['DepletedTranscript','OverExpressedTranscript']].sum().reset_index()
+    thresh['LowProteasome'] = thresh['DepletedTranscript'] > len(Proteasome)/2
+    thresh['HighProteasome'] = thresh['OverExpressedTranscript'] > len(Proteasome)/2
+    clin = clin.merge(thresh, left_on='bcr_patient_barcode', right_on='Barcode')
+    clin.to_csv(DataDir + 'AnnotatedTCGAClinicalOutcomes.txt')
+
 
 def GetMMRFData(DataType):
     '''
@@ -51,55 +80,95 @@ def GetMMRFData(DataType):
     return(Out)
 
 
-def GetCoxRegressionInput():
+def GetCoxRegressionInput(Dataset):
     '''
     Filtering patients for clinical analysis that:
     1) Have had no prior therapies (i.e. this treatment is the first line therapy)
     '''
-    muts = GetMMRFData('Mutations')
-    muts['Barcode'] = muts['Tumor_Sample_Barcode'].str[0:9]
-    df = GetMMRFData(DataType='Clinical')
-    df = df.query("regimen_or_line_of_therapy == 'First line of therapy'")
-    df = df.replace({"'--" : np.nan}).dropna(axis=1, how='all') # remove columns that don't have info
-    df = df.merge(muts, left_on='case_submitter_id', right_on='Barcode')
-    NumTreatPerPatient = df.groupby(['case_submitter_id'])['therapeutic_agents'].size().reset_index().rename(
-        columns={'therapeutic_agents':'NumberOfDiffTreatments'})
-    MetaData = df[['iss_stage','gender','case_submitter_id','age_at_index','days_to_last_follow_up','vital_status']].drop_duplicates().rename(
-        columns={'gender':'sex'})
-    MetaData['survival'] = MetaData['vital_status'].replace('Alive',0).replace('Dead',1) # recode survival as 0 or 1
-    MetaData = MetaData.merge(NumTreatPerPatient, left_on='case_submitter_id', right_on='case_submitter_id')
-    df['ID'] = 1
-    pivoted = pd.pivot_table(df, values='ID', index=['case_submitter_id','MutLoad'], columns = 'therapeutic_agents').reset_index().replace(np.nan,0)
-    # Has to have received one of the 3 proteasome inhibitors in this cohort
-    pivoted = pivoted[(pivoted['Bortezomib'] > 0) | (pivoted['Carfilzomib'] > 0) | (pivoted['Ixazomib'] > 0)]
-    pivoted = pivoted.merge(MetaData, left_on='case_submitter_id', right_on='case_submitter_id')
-    return(pivoted)
+    if Dataset == 'MMRF':
+        muts = GetMMRFData('Mutations')
+        muts['Barcode'] = muts['Tumor_Sample_Barcode'].str[0:9]
+        df = GetMMRFData(DataType='Clinical')
+        df = df.query("regimen_or_line_of_therapy == 'First line of therapy'")
+        df = df.replace({"'--" : np.nan}).dropna(axis=1, how='all') # remove columns that don't have info
+        df = df.merge(muts, left_on='case_submitter_id', right_on='Barcode')
+        NumTreatPerPatient = df.groupby(['case_submitter_id'])['therapeutic_agents'].size().reset_index().rename(
+            columns={'therapeutic_agents':'NumberOfDiffTreatments'})
+        MetaData = df[['iss_stage','gender','case_submitter_id','age_at_index','days_to_last_follow_up','vital_status']].drop_duplicates().rename(
+            columns={'gender':'sex'})
+        MetaData['survival'] = MetaData['vital_status'].replace('Alive',0).replace('Dead',1) # recode survival as 0 or 1
+        MetaData = MetaData.merge(NumTreatPerPatient, left_on='case_submitter_id', right_on='case_submitter_id')
+        df['ID'] = 1
+        pivoted = pd.pivot_table(df, values='ID', index=['case_submitter_id','MutLoad'], columns = 'therapeutic_agents').reset_index().replace(np.nan,0)
+        # Has to have received one of the 3 proteasome inhibitors in this cohort
+        pivoted = pivoted[(pivoted['Bortezomib'] > 0) | (pivoted['Carfilzomib'] > 0) | (pivoted['Ixazomib'] > 0)]
+        pivoted = pivoted.merge(MetaData, left_on='case_submitter_id', right_on='case_submitter_id')
+        return(pivoted)
+    elif Dataset == 'TCGA':
+        df = pd.read_csv('/labs/ccurtis2/tilk/scripts/protein//Data/Raw/Clinical/AnnotatedTCGAClinicalOutcomes.txt')
+        df = df[df['Group'] == '19S Regulatory Particle']
+        df = df[df['Redaction'] != 'Redacted']
+        df['HighProteasome'] = (df['OverExpressedTranscript'] > 0) & (df['DepletedTranscript'] == 0)
+        df['LowProteasome'] = (df['OverExpressedTranscript'] == 0) & (df['DepletedTranscript'] > 0)
+        #df['OverExpressedFraction'] = df['OverExpressedTranscript']/ (df['OverExpressedTranscript'] + df['DepletedTranscript'])
+        #df['DepletedFraction'] = df['DepletedTranscript']/ (df['OverExpressedTranscript'] + df['DepletedTranscript'])
+        #df['HighProteasome'] = df['OverExpressedFraction'] > 0.5
+        #df['LowProteasome'] = df['DepletedFraction'] > 0.5
+        # df['HighProteasome'] = (df['OverExpressedTranscript'] > 0))
+        # df['LowProteasome'] = ( df['DepletedTranscript'] > 0 ) & (df['OverExpressedTranscript'] == 0)
+        df['MutLoadGroup'] = np.where(df['MutLoad'] <= 10, 'Low', np.NaN)
+        df['MutLoadGroup'] = np.where(df['MutLoad'] >= 1000 , 'High', df['MutLoadGroup'])
+        #df = df[(df['LowProteasome'] == True) | (df['HighProteasome'] == True)]
+        df['ProteasomeGroup'] = np.where((df['LowProteasome'] == True), 'LowProteasome', '')
+        df['ProteasomeGroup'] = np.where((df['HighProteasome'] == True), 'HighProteasome', df['ProteasomeGroup'])
+        df['StrataGroup'] = np.where((df['LowProteasome'] == True) & (df['MutLoadGroup'] == 'Low'), 'LowPro_LowTMB','')
+        df['StrataGroup'] = np.where((df['LowProteasome'] == True) & (df['MutLoadGroup'] == 'High'), 'LowPro_HighTMB', df['StrataGroup'])
+        df['StrataGroup'] = np.where((df['HighProteasome'] == True) & (df['MutLoadGroup'] == 'Low'), 'HighPro_LowTMB',df['StrataGroup'])
+        df['StrataGroup'] = np.where((df['HighProteasome'] == True) & (df['MutLoadGroup'] == 'High'), 'HighPro_HighTMB',df['StrataGroup'])
+        df = df[~(df['StrataGroup'] == '')]
+        return(df)
 
 
-def PlotCoxRegression(PlotType):
-    SetUpPlottingPackages()
-    df = GetCoxRegressionInput()
+test = pd.read_csv('/labs/ccurtis2/tilk/scripts/protein/Data/AS_Tables/TCGA_AvgGeneExpInProteasomeForAllCancerTypes')
+test2 = pd.concat([test.groupby(['Barcode'])['DepletedTranscript'].value_counts().unstack().reset_index().replace(np.nan,0).assign(group='DepletedTranscript'),
+                   test.groupby(['Barcode'])['OverExpressedTranscript'].value_counts().unstack().reset_index().replace(np.nan,0).assign(group='OverExpressedTranscript')])
+
+Samples = pd.read_csv(os.getcwd() + '/Data/Raw/Mutations/TCGA/CancerTypesAndMutLoadForDGE')
+Samples['Barcode'] = Samples['Barcode'].str[0:12]
+test3 = test2.merge(Samples, left_on='Barcode', right_on='Barcode')
+
+
+def PlotCoxRegression(PlotType, Dataset):
+    SetUpSurvivalPackages()
+    df = GetCoxRegressionInput(Dataset)
     if PlotType == 'ForestPlot':
         R = ConvertPandasDFtoR(df.astype(str))
-        ro.r.GetForestPlot(R)
+        SetUpSurvivalPackages();ro.r.GetForestPlot(R, Dataset)
+        ro.r.GetForestPlot(R, Dataset)
     elif PlotType == 'BroadLoad':
         df['MutBin'] = pd.cut(df['MutLoad'], bins=[0,50, 500,5600], labels=['0-50' ,'50-500','500-5000'])
         R = ConvertPandasDFtoR(df.astype(str))
-        ro.r.PlotSurvivalCurve(R)
+        ro.r.PlotSurvivalCurve(R, Dataset)
 
 
 
-df = GetCoxRegressionInput()
-df['MutBin'] = pd.cut(df['MutLoad'], bins=[0,50, 500,5600], labels=['0-50' ,'50-500','500-5000'])
-R = ConvertPandasDFtoR(df.astype(str))
+
+GetTCGAClinical()
+
+# R =ConvertPandasDFtoR(GetCoxRegressionInput(Dataset).astype(str))
+# SetUpSurvivalPackages();ro.r.PlotSurvivalCurve(R, Dataset)
 
 
+# R =ConvertPandasDFtoR(GetCoxRegressionInput(Dataset).astype(str))
+# SetUpSurvivalPackages();ro.r.GetForestPlot(R, Dataset)
 
-SetUpPlottingPackages()
-ro.r.PlotSurvivalCurve(R)
 
-SetUpPlottingPackages()
-ro.r.GetForestPlot(R)
+# m=GetCoxRegressionInput(Dataset='MMRF')
+# test= m[(m['NumberOfDiffTreatments'] <= 3)]
+# test['MutLoadGroup'] = np.where(test['MutLoad'] < 100, 'Low', np.NaN)
+# test['MutLoadGroup'] = np.where(test['MutLoad'] >= 100 , 'High', test['MutLoadGroup'])
+# R = ConvertPandasDFtoR(test.astype(str))
+# SetUpSurvivalPackages();ro.r.PlotSurvivalCurve(R, 'MMRF')
 
 # # Remove drugs that have opposite effect of PI that induce 
 # # pivoted = pivoted[(pivoted['Lenalidomide'] == 0) & (pivoted['Thalidomide'] == 0) & (pivoted['Pomalidomide'] == 0)]
