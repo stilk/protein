4e0648e1e4d76db7f61de1e8ed4595e99cab89ba
Susanne Tilk <susannetilk@gmail.com>
2021-11-04 14:50:55 -0700

    add as t-test + grouped reg

--- CalculateMetrics.py
+++ CalculateMetrics.py
@@ -9,7 +9,7 @@ from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
 from GetData import * 
 
-def SetUpPlottingPackages():
+def SetUpRegressionPackages():
     ggplot = importr('ggplot2')
     #scales = importr('scales')
     #ggpmisc = importr('ggpmisc')
@@ -71,13 +71,13 @@ def GetRegressionStatsInput(Dataset, DataType, MutType):
 
 
 def GetRegressionEstimates(Dataset, DataType, MutType='SNV', GeneSet=[], ByGene=True, NormalizeY=True):
-    SetUpPlottingPackages()
+    SetUpRegressionPackages()
     df = GetRegressionStatsInput(Dataset, DataType, MutType)
     if len(GeneSet) != 0: # Subset to genes of interest
         df = df[df['GeneName'].isin(GeneSet)]
     R = ConvertPandasDFtoR(df)
     if ByGene:
-        if DataType == 'Protein' or DataType == 'AS':
+        if DataType == 'Protein' or DataType == 'AS' or DataType == 'RNAi':
             return(ConvertRDataframetoPandas(ro.r.DoRegressionPerGene(R, 'OLS', False)))
         elif Dataset == 'CCLE':
             return(ConvertRDataframetoPandas(ro.r.DoRegressionPerGene(R, 'OLS', True)))
@@ -88,32 +88,83 @@ def GetRegressionEstimates(Dataset, DataType, MutType='SNV', GeneSet=[], ByGene=
     else: # Do regression by gene group
         if Dataset == 'TCGA':
             return(ConvertRDataframetoPandas(ro.r.DoRegressionPerGroup(R)))
+        #elif Dataset == 'CCLE':
 
+
+def GetSubsetOfGeneAnnotationsOfInterest():
+    corum = corum = pd.read_csv('/labs/ccurtis2/tilk/02_DNDS/separateDatabaseFiles/CORUM/coreComplexes.txt.zip', sep='\t')
+    chap = pd.read_csv('/labs/ccurtis2/tilk/09_PROTEIN/Human_Chaperome_TableS1A_PMID_29293508', sep='\t')
+    groups = pd.concat([
+        pd.DataFrame({'Group': 'Cytoplasmic Ribosomes','subgroup' : 'Translation', 'Hugo' : corum[corum['ComplexName'].str.contains('subunit, cytoplasmic')]['subunits(Gene name)'].str.split(';', expand=True).melt()['value'].drop_duplicates().tolist()}),
+        pd.DataFrame({'Group': 'Mitochondrial Ribosomes', 'subgroup' : 'Translation', 'Hugo' : corum[corum['ComplexName'].str.contains('subunit, mitochondrial')]['subunits(Gene name)'].str.split(';', expand=True).melt()['value'].drop_duplicates().tolist()}),
+        pd.DataFrame({'Group': 'Mitochondrial Chaperones' , 'subgroup' : 'Chaperones', 'Hugo' :chap[(chap['Level2'] == 'MITO') & (chap['CHAP / CO-CHAP'] == 'CHAP') ]['Gene']}),
+        pd.DataFrame({'Group': '20S Core' , 'subgroup' : 'Proteasome', 'Hugo' : corum[corum['ComplexName'] == '20S proteasome']['subunits(Gene name)'].str.split(';', expand=True).melt()['value']  }),
+        pd.DataFrame({'Group': '19S Regulatory Particle' , 'subgroup' : 'Proteasome', 'Hugo' : ['PSMC1','PSMC2','PSMC3','PSMC4','PSMC5','PSMC6', 'PSMD1','PSMD2','PSMD3','PSMD4','PSMD6','PSMD7','PSMD8','PSMD11','PSMD12','PSMD13','PSMD14'] }),
+        pd.DataFrame({'Group': 'Small HS' , 'subgroup' : 'Chaperones', 'Hugo' :chap[(chap['Level2'] == 'sHSP') & (chap['CHAP / CO-CHAP'] == 'CHAP') ]['Gene']}),
+        pd.DataFrame({'Group': 'HSP 100' ,'subgroup' : 'Chaperones', 'Hugo' :chap[(chap['Level2'] == 'HSP100') & (chap['CHAP / CO-CHAP'] == 'CHAP') ]['Gene']}),
+        pd.DataFrame({'Group': 'HSP 90', 'subgroup' : 'Chaperones', 'Hugo' :chap[(chap['Level2'] == 'HSP90') & (chap['CHAP / CO-CHAP'] == 'CHAP') ]['Gene']}),
+        pd.DataFrame({'Group': 'HSP 70','subgroup' : 'Chaperones', 'Hugo' : chap[(chap['Level2'] == 'HSP70') & (chap['CHAP / CO-CHAP'] == 'CHAP') ]['Gene']}),
+        pd.DataFrame({'Group': 'HSP 60','subgroup' : 'Chaperones', 'Hugo' : chap[(chap['Level2'] == 'HSP60') & (chap['CHAP / CO-CHAP'] == 'CHAP') ]['Gene']}),
+        pd.DataFrame({'Group': 'HSP 40', 'subgroup' : 'Chaperones','Hugo' : chap[(chap['Level2'] == 'HSP40') & (chap['CHAP / CO-CHAP'] == 'CHAP') ]['Gene']})])
+    return(groups)
+
+def GetshRNARegression():
+    SetUpRegressionPackages()
+    df = GetRegressionStatsInput(Dataset='CCLE', DataType='RNAi', MutType='KsKa')
+    groups = GetSubsetOfGeneAnnotationsOfInterest()
+    df = df.merge(groups, left_on='GeneName', right_on='Hugo')
+    Out = pd.DataFrame()
+    for Group in groups['Group'].unique():
+        SingleGroup = ConvertPandasDFtoR(df[df['Group'] == Group].dropna()[['LogScore','Value','GeneName']])
+        #Reg= ConvertRDataframetoPandas(ro.r.DoLinearRegression(SingleGroup, NormalizeY=True)).reset_index()
+        Reg = ConvertRDataframetoPandas(ro.r.DoOLSRegressionWithGeneName(SingleGroup, False)).reset_index()
+        Reg = pd.concat([df.query('Group == @Group')[['subgroup','Group']].head(2).reset_index(), 
+            Reg.rename(columns={'index':'Coefficient'})], axis=1) ## Add drug name and group to regression output
+        Out = Out.append(Reg)
+    Out = Out.dropna()
+    Out = Out[Out['Coefficient'] == 'LogScore']
+    return(Out)
+
+
+def GetPerDrugRegression():
+    '''Performs linear regression on CCLE lines with load as the predictor and viability at the y.''' 
+    SetUpRegressionPackages()
+    df = GetRegressionStatsInput(Dataset='CCLE', DataType='Drug', MutType='KsKa')
+    Out = pd.DataFrame()
+    for Drug in df['name'].unique():
+        SingleDrug = ConvertPandasDFtoR(df[df['name'] == Drug].dropna())
+        Reg= ConvertRDataframetoPandas(ro.r.DoLinearRegression(SingleDrug, NormalizeY=True)).reset_index()
+        Reg = pd.concat([df.query('name == @Drug')[['name','subgroup','Group']].head(2).reset_index(), 
+            Reg.rename(columns={'index':'Coefficient'})], axis=1) ## Add drug name and group to regression output
+        Out = Out.append(Reg)
+    return(Out)
+
+
+def DoTTestForAS(row):
+    '''This is a two-sided test for the null hypothesis that 2 independent samples have identical average (expected) values.'''
+    Samples = pd.read_csv(os.getcwd() + '/Data/Raw/Mutations/TCGA/CancerTypesAndMutLoadForDGE')
+    Samples['Barcode'] = Samples['Barcode'].str[0:12].str.replace('-','_')
+    Low = row[list(set(row.index) & set(Samples[Samples['MutLoad'] <= 10]['Barcode'].tolist()))].dropna()
+    High = row[list(set(row.index) & set(Samples[Samples['MutLoad'] >= 1000]['Barcode'].tolist()))].dropna()
+    return( pd.Series({'pVal': stats.ttest_ind(Low, High)[1], 
+						'Low_mean': Low.mean(), 
+						'High_mean': High.mean(), 
+						'LowToHighDelta': Low.mean() - High.mean()}))
 
 
 def GetDeltaPSIForEachGene():
     Samples = pd.read_csv(os.getcwd() + '/Data/Raw/Mutations/TCGA/CancerTypesAndMutLoadForDGE')
+    Samples['Barcode'] = Samples['Barcode'].str[0:12].str.replace('-','_')
+    LowAndHigh = Samples[Samples['MutLoad'] < 10]['Barcode'].tolist() + Samples[Samples['MutLoad'] > 1000]['Barcode'].tolist()
+    AS = GetAlternativeSplicingData(AS='RI', BarcodesOfInt=LowAndHigh)
+    # Remove AS events if more than 25% of samples have a missing call
+    PercentMissing = AS.isnull().sum(axis=1).reset_index()[0]/len(AS.columns)
+    AS = AS.reset_index().assign(pct_missing = PercentMissing)
+    AS = AS[AS['pct_missing'] < 0.25]
+    # Get significantly different genes
+    df = AS.apply(DoTTestForAS, axis=1).assign(GeneName=AS['symbol'])
+    return(df)
 
-Low = GetAlternativeSplicingData(AS='RI', BarcodesOfInt=Samples[Samples['MutLoad'] <= 10]['Barcode'].tolist())
-High = GetAlternativeSplicingData(AS='RI', BarcodesOfInt=Samples[Samples['MutLoad'] >= 1000]['Barcode'].tolist())
-
-# def doTTest(row):
-# 	'''This is a two-sided test for the null hypothesis that 2 independent samples have identical average (expected) values.'''
-# 	low = row[list(set(row.index) & set(getBarcodesOfInterest('low_SNV')))].dropna()
-# 	high = row[list(set(row.index) & set(getBarcodesOfInterest('high_SNV')))].dropna()
-# 	return( pd.Series({'pVal': stats.ttest_ind(low, high)[1], 
-# 						'low_mean':low.mean(), 
-# 						'high_mean': high.mean(), 
-# 						'lowToHighDelta': low.mean() - high.mean()
-# 						})
-# 			)
-
-# def calculateDeltaPSI(splice):
-# 	splice = addPctMissing(splice)
-# 	splice = splice[splice['pct_missing'] < 0.25]
-# 	splice = splice.set_index(['symbol', 'as_id', 'splice_type', 'exons', 'from_exon', 'to_exon', 'pct_missing'])
-# 	out = splice.apply(doTTest, axis=1)
-# 	return(out)
 
 
 
@@ -129,12 +180,12 @@ High = GetAlternativeSplicingData(AS='RI', BarcodesOfInt=Samples[Samples['MutLoa
 # GetRegressionEstimates(Dataset='CCLE', DataType='Expression', MutType='KsKa', GeneSet=[], ByGene=True, NormalizeY=True).to_csv(
 #     '/labs/ccurtis2/tilk/scripts/protein/Data/Regression/ExpressionOLSRegressionEstimatesKsKaCCLE')
 
-GetRegressionEstimates(Dataset='CCLE', DataType='Protein', MutType='KsKa', GeneSet=[], ByGene=True, NormalizeY=False).to_csv(
-    '/labs/ccurtis2/tilk/scripts/protein/Data/Regression/ProteinOLSRegressionEstimatesKsKaCCLE')
+# GetRegressionEstimates(Dataset='CCLE', DataType='Protein', MutType='KsKa', GeneSet=[], ByGene=True, NormalizeY=False).to_csv(
+#     '/labs/ccurtis2/tilk/scripts/protein/Data/Regression/ProteinOLSRegressionEstimatesKsKaCCLE')
 
 
-# GetRegressionEstimates(Dataset='CCLE', DataType='RNAi', MutType='KsKa', GeneSet=[], ByGene=True).to_csv(
-#     '/labs/ccurtis2/tilk/scripts/protein/Data/Regression/RNAiOLSRegressionEstimatesKsKaCCLE')
+# GetRegressionEstimates(Dataset='CCLE', DataType='RNAi', MutType='KsKa', GeneSet=[], ByGene=True, NormalizeY=False).to_csv(
+#     '/labs/ccurtis2/tilk/scripts/protein/Data/Regression/AchillesOnlyRNAiOLSRegressionEstimatesNoNormKsKaCCLE')
 
 
 #RunVEPToGetDeleteriousScores(Dataset='CCLE').to_csv('/labs/ccurtis2/tilk/scripts/protein/Data/Raw/Mutations/CCLE/CCLE_Mutations_Polyphen_Annotated', sep='\t')
