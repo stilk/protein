90c13da8e94cf040b1bd4cea9d198d9b46f0b7bf
Susanne Tilk <susannetilk@gmail.com>
2022-02-23 11:09:28 -0800

    add cn and rm unused functions

--- GetData.py
+++ GetData.py
@@ -26,7 +26,7 @@ def GetExpressionData(Dataset, GeneSet=[]):
             output.append(pd.read_csv(FileName, sep='\t').set_index('sample'))
         df = pd.melt(pd.concat(output, axis=1).reset_index(), id_vars=['sample']).rename(columns={'sample':'GeneName', 'value':'Value','variable':'Barcode'})
     elif 'TS' in Dataset:
-        df = pd.read_csv('TCGA_TS.txt')
+        df = pd.read_csv('/oak/stanford/scg/lab_ccurtis2/tilk/scripts/protein/TCGA_TS.txt')
     if len(GeneSet) != 0:
         df = df[df['GeneName'].isin(GeneSet)]
     else:
@@ -109,77 +109,8 @@ def GetRNAiResponseData(GeneSet = []):
     return(ko)		
 
 
-def DownloadFastaForVep():
-	'''
-	To run the --hgvs tag with VEP, a fasta file is required. This function downloads the human
-	genome (GRCh37) Fasta file from Ensembl. For documentation see:
-	https://uswest.ensembl.org/info/docs/webcode/mirror/tools/vep.html
-	'''
-	VEPDir='/labs/ccurtis2/tilk/software/vep/'
-	if os.path.isfile(VEPDir + 'Homo_sapiens.GRCh37.75.dna.toplevel.bgzip.fa.gz') == False:
-		subprocess.call(['bash', '-c', 
-		'wget http://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.75.dna.toplevel.fa.gz' +
-		'-P ' + VEPDir ])
-		# Fasta must be compressed with bgzip, not gzip
-		subprocess.call(['bash', '-c', 'zcat ' + VEPDir + 'Homo_sapiens.GRCh37.75.dna.toplevel.fa.gz | bgzip -c > ' +
-			VEPDir + 'Homo_sapiens.GRCh37.75.dna.toplevel.bgzip.fa.gz'])
-	else:
-		return(print('Fasta already downloaded.'))
 
-def DownloadVEPIndex():
-	'''
-	VEP requires a cached index to run mutation annotations. This function downloads the GRCh37 genom
-	index to your home directory. This downloads a large file (>10G) and takes a very long time run (~9 hrs). 
-	'''	
-	import os
-	import subprocess
-	if os.path.isfile('~/.vep/homo_sapiens_vep_104_GRCh37.tar.gz') == False: # If VEP output doesn't exist yet
-		# Download cached index
-		subprocess.call(['bash', '-c', 
-		'wget http://ftp.ensembl.org/pub/release-104/variation/indexed_vep_cache/homo_sapiens_vep_104_GRCh37.tar.gz -P /labs/ccurtis2/tilk/software/vep'])
-		# Un-tar package
-		subprocess.call(['bash','-c', 'tar -zxvf /labs/ccurtis2/tilk/software/vep/homo_sapiens_vep_104_GRCh37.tar.gz'])
-		return(print('Finished downloading index.'))
-	else:
-		return(print('Index already created.'))
-
-
-def RunVEPToGetDeleteriousScores(Dataset):
-	'''
-	Runs VEP (Variant Effect Predictor) to obtain polyphen/sift scores for CCLE
-	Required columns to run VEP are: Chromosome, Start, Stop, Ref/Alt, Strand
-	'''
-	if Dataset == 'CCLE':
-		muts = pd.read_csv( os.getcwd() + "/Data/Raw/Mutations/CCLE/CCLE_mutations.csv")
-		muts['Ref/Alt'] = muts['Reference_Allele'] + '/' + muts['Tumor_Seq_Allele1']
-	elif Dataset == 'GTEX':
-		muts = GetMutations(Dataset='GTEX')
-		muts = muts.rename(columns={'chr':'Chromosome','pos': 'Start_position'})
-		muts['Chromosome'] = muts['Chromosome'].str.replace('chr','')
-		muts['Start_position'] = muts['Start_position'].astype(int)
-		muts['Ref/Alt'] = muts['ref'] + '/' + muts['alt']
-		muts['End_position'] = muts['Start_position'] # Just SNVs so positions are the same
-		muts['Strand'] = '+' # Everything is positive stranded
-	# Generate VEP Input to file
-	VEPInput = muts[['Chromosome','Start_position','End_position','Ref/Alt','Strand']]
-	VEPInput.to_csv(os.getcwd() + "/Data/Raw/Mutations/" + Dataset + "/" + Dataset + "_VEP_Input" , sep= ' ', index=False, header=None)
-	# Run VEP
-	#if os.path.isfile(os.getcwd() + "/Data/Raw/Mutations/" + Dataset + "/" + Dataset + "_VEP_Output") == False: # If VEP output doesn't exist yet
-	#DownloadVEPIndex()
-	subprocess.call(['bash', '-c', 
-		'vep -e -v --database --dir /labs/ccurtis2/tilk/software/vep/ --species homo_sapiens -i ' + 
-		os.getcwd() + "/Data/Raw/Mutations/" + Dataset + '/' + Dataset + "_VEP_Input" + ' -o ' + os.getcwd() + "/Data/Raw/Mutations/" 
-		+ Dataset + "/" + Dataset + "_VEP_Output " + '--sift --polyphen --format guess --force_overwrite'])
-	#VEPOutput = pd.read_csv(os.getcwd() + "/Data/Raw/Mutations/" + Dataset + "/" + Dataset + "_VEP_Output")
-	print('Done!')
-	#return(pd.concat([muts, VEPOutput], axis=1))
-
-def GetStability(Dataset):
-	DataDir = "/labs/ccurtis2/tilk/scripts/protein/Data/MutFunc/"
-	if Dataset == 'TCGA':
-		return(pd.read_csv(DataDir + 'TCGA_ProteinStability_HomologyModel'))
-
-def GetMutations(Dataset):
+def GetPointMutations(Dataset):
     DataDir = os.getcwd() + '/Data/Raw/Mutations/' + Dataset + '/'
     if 'TCGA' in Dataset:
         muts = pd.read_csv(DataDir + "mc3.v0.2.8.PUBLIC.maf.gz", sep='\t', usecols = ['Variant_Classification','Tumor_Sample_Barcode','Hugo_Symbol','PolyPhen']).rename(
@@ -190,10 +121,26 @@ def GetMutations(Dataset):
         muts = pd.read_csv(DataDir + "CCLE_mutations.csv", sep=',', usecols = ['DepMap_ID','Variant_Classification','Hugo_Symbol']).rename(
             columns={'DepMap_ID': 'Barcode'})
         muts = muts[~muts['Variant_Classification'].isin(['In_Frame_Ins','In_Frame_Del'])] # Don't look at MNVs
-    elif 'GTEX' in Dataset:
-        muts = pd.read_csv(DataDir + 'Table_mutations_all_Garcia_Nieto_et_al_Genome_Biology.tsv', sep='\t').rename(
-            columns={'gtexIds_samples':'Barcode'})
     return(muts)
+
+
+def GetCopyNumberMutations(Dataset):
+	DataDir = os.getcwd() + '/Data/Raw/Mutations/' + Dataset + '/'
+	if Dataset == 'TCGA':
+		SampleInfo = pd.read_csv(DataDir + 'CosmicSample.tsv.gz', sep='\t') # Downloaded on 04/07/2020 v91
+		cnvs = pd.read_csv(DataDir + 'CosmicCompleteCNA.tsv.gz', sep='\t', usecols=['SAMPLE_NAME','MUT_TYPE'])
+		cnvs = cnvs[cnvs['SAMPLE_NAME'].str.contains('TCGA', na=False)]
+	elif Dataset == 'CCLE':
+		cnvs = pd.read_csv(DataDir + 'CCLE_ABSOLUTE_combined_20181227.txt', sep='\t', # Version CCLE 2019 in DepMap
+				usecols=['depMapID','Modal_HSCN_1','Modal_HSCN_2','Modal_Total_CN', 'LOH','Homozygous_deletion']).rename(columns={'depMapID': 'SAMPLE_NAME'}).drop_duplicates()
+		cnvs['MUT_TYPE'] = np.nan
+		cnvs = cnvs[~((cnvs['Modal_Total_CN'] == 2) & (cnvs['Modal_HSCN_1'] == 1) & (cnvs['Modal_HSCN_2'] == 1))] ### remove probes that are a CN of 2
+		cnvs.loc[cnvs['Modal_Total_CN'] > 2, 'MUT_TYPE'] = 'gain' ### annotate amplification similar to COSMIC notation
+		cnvs.loc[cnvs['Modal_Total_CN'] < 2, 'MUT_TYPE'] = 'loss' ### annotate deletion similar to COSMIC notation
+		#cnvs.loc[cnvs['Homozygous_deletion'] == 1, 'MUT_TYPE'] = 'loss' ### annotate homozygous deletion similar to COSMIC notation
+	return(cnvs.groupby(['SAMPLE_NAME'])['MUT_TYPE'].value_counts().reset_index(name='counts').pivot(
+			index='SAMPLE_NAME', columns='MUT_TYPE', values='counts').reset_index().replace(np.nan,0))
+
 
 
 
@@ -204,9 +151,10 @@ def AnnotateMutationalLoad(muts, MutType):
 		muts = pd.concat([muts, pd.DataFrame(KaOrKs) * 1], axis=1)
 		muts['TotalKaKs'] = muts['Ka'] + muts['Ks']
 		MutationRate = muts.groupby('Barcode')['TotalKaKs'].sum().reset_index().rename(columns={'TotalKaKs':'MutLoad'})
-	elif MutType == 'CNV_Altered':
-		chrom = pd.read_csv('/labs/ccurtis2/tilk/02_DNDS/CN_Analysis/02_geneSets/annotations_dEdI/biomart_chromosomes.csv' , sep=',')
-		MutationRate = pd.DataFrame(cn.groupby('Barcode')['Length'].sum()/(chrom['Length'].sum().astype(int))).reset_index().rename(columns={'Length':'MutLoad'})
+	elif MutType == 'CN_Deletions':
+		MutationRate = muts[['SAMPLE_NAME','gain']].rename(columns={'SAMPLE_NAME':'Barcode','gain':'MutLoad'})
+	elif MutType == 'CN_Amplifications':
+		MutationRate = muts[['SAMPLE_NAME','loss']].rename(columns={'SAMPLE_NAME':'Barcode','loss':'MutLoad'})
 	elif MutType == 'Deleterious':
 		MutationRate = muts[muts['Deleterious_Meta'] == True].groupby('Barcode').size().reset_index().rename(columns={0:'MutLoad'})
 	elif MutType == 'SNV': # all SNV mutations
@@ -218,17 +166,7 @@ def AnnotateMutationalLoad(muts, MutType):
 		muts['PolyPhen'] = muts['PolyPhen'].str.split("(", expand=True)[0]
 		muts = muts[muts['PolyPhen'].isin(['probably_damaging', 'possibly_damaging'])]
 		MutationRate = muts.groupby('Barcode').size().reset_index().rename(columns={0:'MutLoad'})
-	elif MutType == 'NonsynonymousHE':
-		print('foo')
 	return(MutationRate)
-
-def GetHighlyExpressedGenes():
-    ''' Defined as having TPM greater than 1000 across all samples in GTEX.'''
-    OutDir='/labs/ccurtis2/tilk/scripts/cancer-HRI/Data/GeneSets/'
-    samp=pd.read_csv(OutDir + 'GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct.gz', skiprows=2, sep='\t')
-    samp['Name'] = samp['Name'].str.split('.', expand=True)[0]
-    genes = samp.set_index(['Description','Name'])
-    return(genes.loc[(genes > 500).all(axis=1)].reset_index()[['Description','Name']])
 
 
 def GetTumorPurity():
@@ -255,7 +193,7 @@ def GetTissueType(Dataset):
     
 def GetNumberOfMutsAndCancerType(Dataset='TCGA'):
 	if Dataset == 'TCGA':
-		muts = GetMutations(Dataset='TCGA')
+		muts = GetPointMutations(Dataset='TCGA')
 		mut_rate = AnnotateMutationalLoad(muts, MutType='KsKa')
 		tissue = GetTissueType(Dataset)[['Barcode','type']]
 		tissue.merge(mut_rate, left_on='Barcode' ,right_on='Barcode').to_csv(
@@ -263,20 +201,4 @@ def GetNumberOfMutsAndCancerType(Dataset='TCGA'):
 		)
 
 
-def GetTotalNMDFraction():
-	nmd = pd.read_csv('/labs/ccurtis2/tilk/09_PROTEIN/splicingAnalysis/NMD/ncomms15943-s2.txt', sep='\t', skiprows=1)
-	mutrate = pd.read_csv(os.getcwd() + '/Data/Raw/Mutations/TCGA/CancerTypesAndMutLoadForDGE', sep=',')
-	drivers = pd.read_csv('/labs/ccurtis2/tilk/scripts/cancer-HRI/Data/GeneSets/drivers_Bailey2018.txt', header=None)[0]
-	nmd['driver'] = nmd['gene'].isin(drivers)
-	mutrate = mutrate.merge(nmd, right_on='sample', left_on='Barcode')
-	mutrate['xbin'] = pd.cut(mutrate['MutLoad'], bins=[0,10,100,1000,30000])
-	nmd = mutrate.groupby(['xbin','driver'])['prediction'].value_counts().unstack().reset_index().replace(np.nan, 0)
-	nmd['fraction'] = nmd['NMD-elicit']/(nmd['NMD-escape'] + nmd['NMD-elicit'])
 
-
-
-
-
-
-
-#RunVEPToGetDeleteriousScores(Dataset='GTEX').to_csv('/labs/ccurtis2/tilk/scripts/protein/Data/Raw/Mutations/GTEX/GTEX_Mutations_Polyphen_Annotated', sep='\t')
