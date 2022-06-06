18eb60ea66354c7002fd8fc62a6e949ca6087e7b
Susanne Tilk <susannetilk@gmail.com>
2022-06-06 08:36:53 -0700

    split rpy2 importing into different script

---
+++ GetRCodeIntoPython.py
@@ -0,0 +1,42 @@
+'''
+This script contains all the functions used to import R code into Python with the rpy2 package.
+All code written in R is used for gene set enrichment analysis, visualization and statistics.
+'''
+
+import os
+import rpy2.robjects as ro 
+from rpy2.robjects.packages import importr
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
+from rpy2.robjects.vectors import FloatVector
+
+def SetUpRegressionPackages():
+    lme4 = importr('lme4')
+    glmnet = importr('glmnet')
+    lmertest = importr('lmerTest')
+    data_table = importr('data.table')
+    dplyr = importr('dplyr')
+    ro.r.source(os.getcwd() + '/GetRegressionStats.R') # Source R functions for running regression models
+
+def SetUpPlottingPackages():
+    ggplot = importr('ggplot2')
+    cowplot = importr('cowplot')
+    lme4 = importr('lme4')
+    glmnet = importr('glmnet')
+    lmertest = importr('lmerTest')
+    data_table = importr('data.table')
+    dplyr = importr('dplyr')
+    gprofiler = importr('gprofiler2')
+    ro.r.source(os.getcwd() + '/Plotting.R') # Source R script for plotting
+    ro.r.source(os.getcwd() + '/DoGeneSetEnrichment.R') # Source R script for plotting
+
+
+def ConvertPandasDFtoR(df):
+    with localconverter(ro.default_converter + pandas2ri.converter): 
+        dfInR = ro.conversion.py2rpy(df) # Convert pandas dataframe to R 
+    return(dfInR)
+
+def ConvertRDataframetoPandas(df):
+    with localconverter(ro.default_converter + pandas2ri.converter):
+        dfInPd = ro.conversion.rpy2py(df) # Convert R dataframe back to Pandas
+    return(dfInPd)
