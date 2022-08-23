c33f85ebbb3c56e6e082063f7cfc40b80f099e26
stilk <susannetilk@gmail.com>
2022-08-23 11:19:45 -0700

    add options for model diagnostics + different regression model opts

--- GetRegressionStats.R
+++ GetRegressionStats.R
@@ -17,34 +17,66 @@ NormalizeValues = function(df) {
 } 
 
 
-DoMixedEffectRegression = function(df, NormalizeY, DoModelDiagnostics=FALSE) { # Used for expression (TCGA)
+DoMixedEffectRegression = function(df, NormalizeY, ModelDiagnostics='None') { # Used for expression (TCGA)
     # Does mixed effect regression of Values by LogScore (i.e. mutational load) while controlling for cancer type and
     # tumor purity. Outputs a dataframe of regression results. 
     # Parameters:
     #   @df = dataframe of raw inputs used for regression. Should have columns for the dependent variables named
     #       'purity','type', and 'LogScore'. Independent variable column is named 'Value'.
     #   @NormalizeY = boolean of whether to normalize y values using @NormalizeValues
-    #   @DoModelDiagnostics = boolean of whether to output results from @DoModelDiagnostics instead
+    #   @ModelDiagnostics = string of which type of model diagnostic to run by @DoModelDiagnostic,
+    #       will not run if string is 'None' 
     if (NormalizeY) {
-         model = lmer(NormalizedValue ~ LogScore + (1 | type) + purity , data=df)
+        if(!("NormalizedValue" %in% names(df))) { # If normalized values don't exist in dataframe, normalize vals
+           df = data.frame(df %>% group_by(GeneName) %>% do(data.frame(NormalizeValues(.))))    
+        }
+        model = lmer(NormalizedValue ~ LogScore + (1 | type) + purity , data=df)
     } else {
-         model = lmer(Value ~ LogScore + (1 | type) + purity, data=df)
+        model = lmer(Value ~ LogScore + (1 | type) + purity, data=df)
     }
-    if (DoModelDiagnostics){
-        return(DoModelDiagnostics(df, model))
-    } else {
+    if (ModelDiagnostics != 'None'){ # Do model diagnostics
+        return(DoModelDiagnostics(df, model,ModelDiagnostics))
+    } else { # Don't do model diagnostics
         return(data.frame(summary(model)[10]$coefficients, summary(model)[11]))
     }
 }
 
 
-DoOLSRegressionWithGeneName = function(df, NormalizeY, DoModelDiagnostics=FALSE) { # Used for shRNA
+
+
+DoMixedEffectRegressionWithoutPurity = function(df, NormalizeY, ModelDiagnostics='None') { # Used for expression (TCGA)
+    # Does mixed effect regression of Values by LogScore (i.e. mutational load) while controlling for cancer type and
+    # tumor purity. Outputs a dataframe of regression results. 
+    # Parameters:
+    #   @df = dataframe of raw inputs used for regression. Should have columns for the dependent variables named
+    #       'purity','type', and 'LogScore'. Independent variable column is named 'Value'.
+    #   @NormalizeY = boolean of whether to normalize y values using @NormalizeValues
+    #   @ModelDiagnostics = string of which type of model diagnostic to run by @DoModelDiagnostic,
+    #       will not run if string is 'None' 
+    if (NormalizeY) {
+        if(!("NormalizedValue" %in% names(df))) { # If normalized values don't exist in dataframe, normalize vals
+           df = data.frame(df %>% group_by(GeneName) %>% do(data.frame(NormalizeValues(.))))    
+        }
+        model = lmer(NormalizedValue ~ LogScore + (1 | type) , data=df)
+    } else {
+        model = lmer(Value ~ LogScore + (1 | type) , data=df)
+    }
+    if (ModelDiagnostics != 'None'){ # Do model diagnostics
+        return(DoModelDiagnostics(df, model,ModelDiagnostics))
+    } else { # Don't do model diagnostics
+        return(data.frame(summary(model)[10]$coefficients, summary(model)[11]))
+    }
+}
+
+
+DoOLSRegressionWithGeneName = function(df, NormalizeY, ModelDiagnostics='None') { # Used for shRNA
     # Does OLS regression of 'Value' by 'LogScore' and 'GeneName'. Outputs a dataframe of regression results.
     # Parameters:
     #   @df = dataframe of raw inputs used for regression. Should have columns for the dependent variables named
     #       'GeneName', and 'LogScore'. Independent variable column is named 'Value'.
     #   @NormalizeY = boolean of whether to normalize y values using @NormalizeValues
-    #   @DoModelDiagnostics = boolean of whether to output results from @DoModelDiagnostics instead
+    #   @ModelDiagnostics = string of which type of model diagnostic to run by @DoModelDiagnostic,
+    #       will not run if string is 'None' 
     if (NormalizeY) {
         if(!("NormalizedValue" %in% names(df))) { # If normalized values don't exist in dataframe, normalize vals
            df = data.frame(df %>% group_by(GeneName) %>% do(data.frame(NormalizeValues(.))))    
@@ -53,34 +85,67 @@ DoOLSRegressionWithGeneName = function(df, NormalizeY, DoModelDiagnostics=FALSE)
     } else  {
         model = lm(Value ~ LogScore + GeneName, data=df)
     }
-    if (DoModelDiagnostics){
-        return(DoModelDiagnostics(df, model))
-    } else {
+    if (ModelDiagnostics != 'None'){ # Do model diagnostics
+        return(DoModelDiagnostics(df, model,ModelDiagnostics))
+    } else { # Don't do model diagnostics
         f = summary(model)$fstatistic
         pVal = pf(f[1],f[2],f[3],lower.tail=F) # p value for the overall fit of the model againt null that R2=0
         return(data.frame(summary(model)[9], summary(model)[6], summary(model)[4]$coefficients, pVal))
     }
 }
 
-DoLinearRegression = function(df, NormalizeY, DoModelDiagnostics=FALSE) { # Used for drug and exp(CCLE)
+
+
+
+
+
+DoLinearRegressionWithPurity = function(df, NormalizeY, ModelDiagnostics='None') { # Used for protein TCGA
     # Does OLS regression of Values by LogScore (i.e. mutational load) and outputs a dataframe of regression results. 
     # Parameters:
     #   @df = dataframe of raw inputs used for regression. Should have column for the dependent variable named 'LogScore'. 
     #       Independent variable column is named 'Value'.
     #   @NormalizeY = boolean of whether to normalize y values using @NormalizeValues
-    #   @DoModelDiagnostics = boolean of whether to output results from @DoModelDiagnostics instead
+    #   @ModelDiagnostics = string of which type of model diagnostic to run by @DoModelDiagnostic,
+    #       will not run if string is 'None' 
+    if (NormalizeY) {
+        if(!("NormalizedValue" %in% names(df))) { # If normalized values don't exist in dataframe, normalize vals
+           df = NormalizeValues(df)
+        }
+        df = NormalizeValues(df)
+        model = lm(NormalizedValue ~  LogScore + purity, data=df)
+    } else { # do not normalize Y values
+        model = lm(Value ~ LogScore + purity, data=df)
+    }
+    if (ModelDiagnostics != 'None'){ # Do model diagnostics
+        return(DoModelDiagnostics(df, model,ModelDiagnostics))
+    } else { # Don't do model diagnostics
+        f = summary(model)$fstatistic
+        pVal = pf(f[1],f[2],f[3],lower.tail=F) # p value for the overall fit of the model againt null that R2=0
+        return(data.frame(summary(model)[9], summary(model)[6], summary(model)[4]$coefficients, pVal))
+    }
+}
+
+
+DoLinearRegression = function(df, NormalizeY, ModelDiagnostics='None') { # Used for drug and exp(CCLE)
+    # Does OLS regression of Values by LogScore (i.e. mutational load) and outputs a dataframe of regression results. 
+    # Parameters:
+    #   @df = dataframe of raw inputs used for regression. Should have column for the dependent variable named 'LogScore'. 
+    #       Independent variable column is named 'Value'.
+    #   @NormalizeY = boolean of whether to normalize y values using @NormalizeValues
+    #   @ModelDiagnostics = string of which type of model diagnostic to run by @DoModelDiagnostic,
+    #       will not run if string is 'None' 
     if (NormalizeY) {
         if(!("NormalizedValue" %in% names(df))) { # If normalized values don't exist in dataframe, normalize vals
            df = NormalizeValues(df)
         }
         df = NormalizeValues(df)
         model = lm(NormalizedValue ~  LogScore, data=df)
-    } else {
+    } else { # do not normalize Y values
         model = lm(Value ~ LogScore, data=df)
     }
-    if (DoModelDiagnostics){
-        return(DoModelDiagnostics(df, model))
-    } else {
+    if (ModelDiagnostics != 'None'){ # Do model diagnostics
+        return(DoModelDiagnostics(df, model,ModelDiagnostics))
+    } else { # Don't do model diagnostics
         f = summary(model)$fstatistic
         pVal = pf(f[1],f[2],f[3],lower.tail=F) # p value for the overall fit of the model againt null that R2=0
         return(data.frame(summary(model)[9], summary(model)[6], summary(model)[4]$coefficients, pVal))
@@ -88,14 +153,15 @@ DoLinearRegression = function(df, NormalizeY, DoModelDiagnostics=FALSE) { # Used
 }
 
 
-DoRegressionPerGene = function(df, RegressionType, NormalizeY, DoModelDiagnostics=FALSE) {
+DoRegressionPerGene = function(df, RegressionType, NormalizeY, ModelDiagnostics='None') {
     # Takes a dataframe of values (e.g., expression/shRNA/drug) and mutational load for each tumor/cell line and 
     # runs @DoLinearRegression of @DoMixedEffectsRegression by each gene in the dataframe.
     # Parameters:
     #   @df = dataframe of raw inputs used for regression
     #   @RegressionType = a string of which regression model to use 'MixedEffect' or 'OLS'
     #   @NormalizeY = boolean of whether to normalize y values using @NormalizeValues
-    #   @DoModelDiagnostics = boolean of whether to output results from @DoModelDiagnostics instead
+    #   @ModelDiagnostics = string of which type of model diagnostic to run by @DoModelDiagnostic,
+    #       will not run if string is 'None' 
     RegressionResults = data.frame()
     AllGenes = unique(df$GeneName)
     for (i in 1:length(AllGenes)) {
@@ -104,12 +170,19 @@ DoRegressionPerGene = function(df, RegressionType, NormalizeY, DoModelDiagnostic
         GeneName = unique(SingleGene$GeneName)
         tryCatch( 
         {  if (RegressionType == 'MixedEffect') {
-                RegressionResults = rbind(RegressionResults, data.frame(DoMixedEffectRegression(SingleGene, NormalizeY, DoModelDiagnostics), GeneName))
+                RegressionResults = rbind(RegressionResults, data.frame(DoMixedEffectRegression(SingleGene, NormalizeY, ModelDiagnostics), GeneName))
         } else if (RegressionType == 'OLS') {
-                RegressionResults = rbind(RegressionResults, data.frame(DoLinearRegression(SingleGene, NormalizeY, DoModelDiagnostics), GeneName))
+                RegressionResults = rbind(RegressionResults, data.frame(DoLinearRegression(SingleGene, NormalizeY, ModelDiagnostics), GeneName))
+        } else if (RegressionType == 'OLSWithPurity') {
+                RegressionResults = rbind(RegressionResults, data.frame(DoLinearRegressionWithPurity(SingleGene, NormalizeY, ModelDiagnostics), GeneName))
+        } else if (RegressionType == 'MixedWithoutPurity') {
+                RegressionResults = rbind(RegressionResults, data.frame(DoMixedEffectRegressionWithoutPurity(SingleGene, NormalizeY, ModelDiagnostics), GeneName))
         }}, 
             error=function(error_message) { 
-            message(error_message)
+            print(message(error_message))
+            write.table(SingleGene, '/home/tilk/DEBUG', quote=FALSE)
+            print(head(SingleGene))
+            #print(RegressionResults)
             print(paste0("Couldn't calculate regression statistics for ", AllGenes[i]))
         })
     }
@@ -118,29 +191,39 @@ DoRegressionPerGene = function(df, RegressionType, NormalizeY, DoModelDiagnostic
 
 
 
-DoModelDiagnostics = function(df, model) {
+DoModelDiagnostics = function(df, model, DiagnosticType='') {
     # Compares fitted values vs residuals in a given regression model to assess heteroscedasticity.
     # Compares residuals in the entire model overall, and residuls against all explanatory variables.
     # Parameters:
     #   @df = dataframe of regression model inputs used (e.g., expression values/load/purity/type for each tumor)
     #   @model = raw regression model output that contains residuals
+    #   @DiagnosticType = If 'SummarizedByAllGenes', will compare fitted values vs residuals in a given regression model to assess heteroscedasticity.
+    #                    summarized as correlation coef. If 'RawValsByComplex', will output raw residuals for genes in complexes of interest.
+    #                   Model diagnostics are performed by @GetRegressionModelDiagnostics. If 'None', will not perform model diagnostics.
+
     Diagnostics = data.frame() # Empty df where results are appended
     df = na.omit(df) # Make sure no NAs exist, which are already omitted during regression 
-    Diagnostics = rbind(Diagnostics, data.frame('PearsonsR' = cor(fitted(model),resid(model), method=c("pearson")),
-        'Diagnostic' = c('FullModel'), 'Type' = c('All'))) # Compare residuals in the entire model
-    Diagnostics = rbind(Diagnostics, data.frame('PearsonsR' = cor(df$LogScore, as.data.frame(summary(model)$residuals)[,1], 
-        method=c("pearson")),'Diagnostic' = c('MutLoad'), 'Type' = c('All'))) # Compare residuals vs all explanatory variables
-    if("purity" %in% colnames(df)) { # Only look at purity as explanatory variable in TCGA (not CCLE)
-        Diagnostics = rbind(Diagnostics, data.frame('PearsonsR' = cor(df$purity, as.data.frame(summary(model)$residuals)[,1],  
-            method=c("pearson")), 'Diagnostic' = c('Purity'), 'Type' = c('All')))     
-        # Compare fitted vs residuals within each cancer type (i.e. full model)
-        WithinGroupComp = data.frame(type = as.character(df$type), fitted = c(fitted(model)), resid = c(resid(model)) )
-        WithinGroupComp = WithinGroupComp %>% group_by(type) %>% summarise(PearsonsR = cor(fitted, resid), method=c("pearson"))
-        Diagnostics = rbind(Diagnostics, data.frame('PearsonsR' = WithinGroupComp$PearsonsR, 'Diagnostic' = c('FullModel'),
-             'Type' = WithinGroupComp$type))   # Type also only for CCLE regression  
+    if (DiagnosticType == 'SummarizedByAllGenes') {
+        Diagnostics = rbind(Diagnostics, data.frame('PearsonsR' = cor(fitted(model),resid(model), method=c("pearson")),
+            'Diagnostic' = c('FullModel'), 'Type' = c('All'))) # Compare residuals in the entire model
+        Diagnostics = rbind(Diagnostics, data.frame('PearsonsR' = cor(df$LogScore, as.data.frame(summary(model)$residuals)[,1], 
+            method=c("pearson")),'Diagnostic' = c('MutLoad'), 'Type' = c('All'))) # Compare residuals vs all explanatory variables
+        if("purity" %in% colnames(df)) { # Only look at purity as explanatory variable in TCGA (not CCLE)
+            Diagnostics = rbind(Diagnostics, data.frame('PearsonsR' = cor(df$purity, as.data.frame(summary(model)$residuals)[,1],  
+                method=c("pearson")), 'Diagnostic' = c('Purity'), 'Type' = c('All')))     
+            # Compare fitted vs residuals within each cancer type (i.e. full model)
+            WithinGroupComp = data.frame(type = as.character(df$type), fitted = c(fitted(model)), resid = c(resid(model)) )
+            WithinGroupComp = WithinGroupComp %>% group_by(type) %>% summarise(PearsonsR = cor(fitted, resid), method=c("pearson"))
+            Diagnostics = rbind(Diagnostics, data.frame('PearsonsR' = WithinGroupComp$PearsonsR, 'Diagnostic' = c('FullModel'),
+                'Type' = WithinGroupComp$type))   # Type also only for CCLE regression  
+        } 
+    } else if (DiagnosticType == 'RawValsByComplex'){
+        Diagnostics = WithinGroupComp = data.frame(gene = as.character(df$GeneName), type = as.character(df$type), 
+            fitted = c(fitted(model)), resid = c(resid(model)) )  # Raw residuals for every gene and cancer type
     }
     return(Diagnostics)
 }
+
 
 
 DoGeneSetEnrichment = function(GeneSet) {
